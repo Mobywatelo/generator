e2f7667cbd8f862cac2b12b86bd4d57e44a50e68
Mobywatelo <g.williams.zgubernator@gmail.com>
2025-04-25 15:33:42 +0200

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -57,7 +57,7 @@ imageInput.addEventListener('change', (event) => {
     fetch('	https://api.imgur.com/3/image' ,{
         method: 'POST',
         headers: {
-            'Authorization': 'Client-ID 4ecc257cbb25ccc'
+            'Authorization': 'Client-ID a1a944465f53de7'
         },
         body: data
     })
